d5cea830bd072cd883d1e4a325d2860787c8263a
Aman10chandra <amanchandra17777777@gmail.com>
2026-01-08 20:46:28 +0530

    Navbar Componenet

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Body from './components/Body.jsx'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-white">
+    <div className="min-h-screen bg-[#312E86] text-white">
       {/*Navbar*/}
       {/*Body here*/}
       {/*footers*/}
